d5349b5b425d39d004de607381cab9b0250564f9
trien <nguyentientrien11t2@gmail.com>
2026-02-27 20:50:42 +0700

    Remove unnecessary local files

--- delete_module_info.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Xóa file module-info.java
-cd /d "d:\FPT-KTPM\ki5\SWP301\Project_LBMS\LibraryManagementWebsite\src\main\java\"
-del module-info.java
-echo File module-info.java đã được xóa!
-pause
--- rebuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Rebuild Maven project
-cd /d "d:\FPT-KTPM\ki5\SWP301\Project_LBMS\LibraryManagementWebsite"
-echo Rebuilding project...
-mvn clean package -DskipTests
-echo Build completed!
-pause
